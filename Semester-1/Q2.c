2827f0690b9faedc7b236f13580b9bbe49716739
Arpita Shaw <144920372+Arpita588@users.noreply.github.com>
2023-10-10 12:08:26 +0530

    Q2.c

---
+++ Semester-1/Q2.c
@@ -0,0 +1,11 @@
+//WAP to print a ASCII value of a given character
+
+#include<stdio.h>
+void main ()
+{
+  char a;
+  printf("Enter a character") ;
+  scanf("%c",&a);
+  printf("ASCII value of %c is %d",a,a);
+}
+    
