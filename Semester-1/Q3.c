f6878445dfb24c9f37a906772d21f73ccac0ebb5
Arpita Shaw <144920372+Arpita588@users.noreply.github.com>
2023-10-10 12:38:02 +0530

    Update Q3.c

--- Semester-1/Q3.c
+++ Semester-1/Q3.c
@@ -5,7 +5,7 @@ void main()
 {
   int A ,P, l,b;
   printf("Enter the values of length and breadth");
-  scanf("%d %d",l,b);
+  scanf("%d %d",&l,&b);
   A= l*b;
   P= 2*(l+b);
   printf("The area of the rectangle is %d and Perimeter of the rectangle is %d",A,P);
